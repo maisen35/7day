af36a518c5bc3474d4f21e0a131db69438ab3eac
maisen35 <suzukixoxb5021@gmail.com>
2022-03-03 06:33:33 +0000

    [Add]controller model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "7c3abf3cf11a4732bc54d043b3ea30bf.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admin,skip: [:registrations,  :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :posts, only: [:destroy]
+  end
+
+  devise_for :users,skip: [:passwords], controllers: {
+    regstrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  scope module: :public do
+    resources :users, only: [:show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
+    resources :posts, only: [:new, :index, :create] do
+      resource :likes, only: [:create, :destroy]
+    end
+  end
+
+  root to: 'homes#top'
+
+
+
 end
