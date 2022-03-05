86e534867d36316cdf6caecc61a0e7e63fc3028e
maisen35 <suzukixoxb5021@gmail.com>
2022-03-05 03:08:59 +0000

    [Add]bootstrap etc

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,13 @@ Rails.application.routes.draw do
   }
 
   scope module: :public do
+    get 'posts/thanks' => 'posts#thanks', as: 'thanks'
     resources :users, only: [:show, :edit, :update] do
       resource :relationships, only: [:create, :destroy]
       get 'followings' => 'relationships#followings', as: 'followings'
       get 'followers' => 'relationships#followers', as: 'followers'
     end
-    resources :posts, only: [:new, :index, :create] do
+    resources :posts, only: [:new, :index, :show, :create] do
       resource :likes, only: [:create, :destroy]
     end
   end
