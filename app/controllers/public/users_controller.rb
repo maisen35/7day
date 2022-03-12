b707ffec4b31f8db0bc5fc271fcb9ae76c2ce81f
maisen35 <suzukixoxb5021@gmail.com>
2022-03-12 10:03:21 +0000

    [Add]layout second

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -19,7 +19,7 @@ class Public::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image, :twitter, :instagram)
   end
 
   def ensure_correct_user
