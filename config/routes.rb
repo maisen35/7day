7c769a6dcf5e01b5b2bc164f49af9fd43d104f67
maisen35 <suzukixoxb5021@gmail.com>
2022-03-16 08:54:22 +0000

    [Add]slide show

--- config/routes.rb
+++ config/routes.rb
@@ -29,10 +29,11 @@ Rails.application.routes.draw do
   end
 
   root to: 'homes#top'
+  get 'about' => 'homes#about', as: 'about'
   resources :contacts, only: [:new, :create]
-  post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
-  post 'contacts/back', to: 'contacts#back', as: 'back'
-  get 'done', to: 'contacts#done', as: 'done'
+  post 'contacts/confirm' =>  'contacts#confirm', as: 'confirm'
+  post 'contacts/back' => 'contacts#back', as: 'back'
+  get 'done' => 'contacts#done', as: 'done'
 
 
 
