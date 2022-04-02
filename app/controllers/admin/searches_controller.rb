7185bb0613545cdb711f4bc06eb740fa38cec4d5
maisen35 <suzukixoxb5021@gmail.com>
2022-04-02 05:05:00 +0000

    [Add]admin_search

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -2,13 +2,14 @@ class Admin::SearchesController < ApplicationController
   before_action :authenticate_admin!
 
   def search
+    @word = params[:words]
     @range = params[:range]
     if @range == "User"
-      @users = User.where("name LIKE(?)", "%#{params[:words]}%")
+      @users = User.where("name LIKE(?)", "%#{params[:words]}%").page(params[:page]).per(10)
     elsif @range == "Tweet"
-      @tweets = Tweet.where("tweet LIKE(?)", "%#{params[:words]}%")
+      @tweets = Tweet.where("tweet LIKE(?)", "%#{params[:words]}%").page(params[:page]).per(10)
     else
-      @contacts = Contact.where("message LIKE(?)", "%#{params[:words]}%")
+      @contacts = Contact.where("message LIKE(?)", "%#{params[:words]}%").page(params[:page]).per(10)
     end
   end
 end
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,11 +2,12 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
+    @word = params[:words]
     @range = params[:range]
     if @range == "User"
-      @users = User.where("name LIKE(?)", "%#{params[:words]}%")
+      @users = User.where("name LIKE(?)", "%#{params[:words]}%").page(params[:page]).per(10)
     else
-      @tweets = Tweet.where("tweet LIKE(?)", "%#{params[:words]}%")
+      @tweets = Tweet.where("tweet LIKE(?)", "%#{params[:words]}%").page(params[:page]).per(10)
     end
   end
 end
